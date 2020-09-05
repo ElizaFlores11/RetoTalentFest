11ce38d94b1965a55e86eaed2bb8d25e398577a3
ElizaFlores11 <60673917+ElizaFlores11@users.noreply.github.com>
2020-09-05 16:01:02 -0500

    Funcionalidad de Detalles Banner

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React, {Fragment} from 'react';
 import 'antd/dist/antd.css';
 import './styles/styles.scss';
 import Routes from './componets/Routes'; 
-import Example from './componets/Calendario/Calendario'
 
 function App() {
   return (
     <Fragment >
-      <Example />
+      <Routes />
     </Fragment>
   );
 }
--- src/componets/DetalleBanner/DetalleBanner.js
+++ src/componets/DetalleBanner/DetalleBanner.js
@@ -1,32 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col, Layout } from 'antd';
 import Logo from '../assets/walmart.png'; 
+
 const { Content } = Layout
 
-const DetalleBanner = () =>{
+const DetalleBanner = ({detalles, detalle, setDetalle}) =>{
+    const {frase1, frase2, frase3, frase4, frase5, frase6, frase7, titulo} = detalles
     return(
         <Layout >
         <Content >
         <Row className="detalle-info tamdetalle">
             <Col span={18} push={6} className="stylerow tamdetalle">
             <Row>
-                <Col span={24}><h2>Anuncia tus promociones</h2></Col>
+                <Col span={24}><h2>{titulo}</h2></Col>
             </Row>
             <Row justify="start" className="detalle-info" >
                 <Col span={8}><h2>icono</h2></Col>
-                <Col span={16}><p>Es una campaña dinámica, con una presentación en carrusel, apareceras en la pagina de inicio de nuesta página web.</p></Col>
+                <Col span={16}><p>{frase1}</p></Col>
             </Row>
             <Row justify="start" className="detalle-info">
                 <Col span={8}><h2>icono</h2></Col>
-                <Col span={16}><p>La ventaja de contratar esta campaña es que estaras visible en el momentoque nuestros clientes ingresen a la página.</p></Col>
+                <Col span={16}><p>{frase2}</p></Col>
             </Row>
             <Row justify="start" className="detalle-info">
                 <Col span={8}><h2>icono</h2></Col>
-                <Col span={16}><p>Ubicación: parte superio de la página <br/>
-                                Medidas de 728×293 pixeles<br/>
-                                El peso de la imagen sera de máximo<br/>
-                                En un formato editable.<br/>
-                                Costo:$$$</p>
+                <Col span={16}>
+                    <p>{frase3}<br/>
+                       {frase4}<br/>
+                       {frase5}<br/>
+                       {frase6}<br/>
+                       {frase7}</p>
                 </Col>
             </Row>
             <Row>
@@ -39,7 +42,9 @@ const DetalleBanner = () =>{
         </Col>
         </Row>
     </Content>
+    <br />
     </Layout>
+ 
     )
 } 
 export default DetalleBanner
