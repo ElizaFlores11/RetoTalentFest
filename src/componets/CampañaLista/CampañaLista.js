c5f276b973b9c744acdba6adb92508f2e4e0d700
Mariel <marielcarrillo@MacBook-Pro-de-Mariel.local>
2020-09-05 13:50:38 -0500

    create component, copys ready but not button added

---
+++ src/componets/CampañaLista/CampañaLista.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Boton from '../'
+
+
+const CampañaLista = ()=> {
+    return(
+        <div>
+            <h1>TU CAMPAÑA ESTA LISTA!</h1>
+            <div>
+                <p>Tu campaña estará lista a partir del día ..., Gracias por ser parte del Walmart!</p>
+            </div>
+        </div>
+    )
+}
