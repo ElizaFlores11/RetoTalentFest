8c053461ba402e8b545f3c72d32845d42b833672
Mariel <marielcarrillo@MacBook-Pro-de-Mariel.local>
2020-09-07 22:42:05 -0500

    home view ready

--- src/componets/Pasos/Pasos.js
+++ src/componets/Pasos/Pasos.js
@@ -2,9 +2,9 @@ import React from 'react';
 import pasos from '../assets/pasos.png'
 const Pasos = ()=> {
     return(
-        <div>
-            <h1>Bienvenido</h1>
-            <p>Como comprar tu campaña publicitaria</p>
+        <div className='pasos-container'>
+            <h1 className='h1-welcome'>Bienvenido</h1>
+            <p className='text' >Como comprar tu campaña publicitaria</p>
             <img src={pasos}></img>
         </div>
     )
