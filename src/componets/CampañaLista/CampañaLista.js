4fe22e0d5aae633e221d60fe49b0d18ce9730293
ElizaFlores11 <60673917+ElizaFlores11@users.noreply.github.com>
2020-09-09 21:13:12 -0500

    componente de modal

--- src/componets/CampañaLista/CampañaLista.js
+++ src/componets/CampañaLista/CampañaLista.js
@@ -1,16 +1,56 @@
-import React from 'react';
-
-
+import React, {useState} from 'react';
+import pasos from '../assets/pasos5.png'
+import { useHistory } from 'react-router-dom'
+import { Modal, Button } from 'antd';
 
 const CampañaLista = ()=> {
+    const [state, setState] =  useState({visible: false})
+    //  state = { visible: false };
+    let history = useHistory()
+    function regresar() {
+
+        
+    }
+    const showModal = () => {
+        setState({
+        visible: true,
+        });
+    };
+    const handleHome = e => {
+        //console.log(e);
+        setState({
+        visible: false,
+        });
+        history.push('/HomeProveedor')
+    };
+    const handleDetalles = e => {
+        //console.log(e);
+        setState({
+        visible: false,
+        });
+        history.push('/DetallesBaamer')
+    };
+    
     return(
         <div>
-            <h1>TU CAMPAÑA ESTA LISTA!</h1>
-            <div>
-                <p>Tu campaña estará lista a partir del día ..., Gracias por ser parte del Walmart!</p>
-                <button>Finalizar</button>
-                <button>Comprar otra campaña</button>
-            </div>
+        <Button type="primary" onClick={showModal}>
+            Open Modal
+        </Button>
+       <Modal
+         visible={state.visible}
+            onOk={handleHome}
+            onCancel={handleDetalles}
+            footer={[
+                <Button id='btnAgregar' value='Agregar' enter key="submit" type="primary" onClick={handleHome}/>
+              ]}
+            >
+         <img src={pasos} alt="Completado" style={{width: '100px'}} /> 
+         <h3>Listo</h3>
+        <p>Tu campaña esta lista para el día 13 de septiembre 2020</p>
+        <p>Gracias por ser parte de Walmart</p>
+        <button className='blue-btn' onClick={handleHome}>Listo</button>   
+        <button className='yellow-btn' onClick={handleDetalles}>+ Agregar espacios Disponibles</button>
+       </Modal>
         </div>
     )
 }
